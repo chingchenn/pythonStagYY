4a902728b41363d72fd8838a2158430009111bc0
chingchenn <ching602504@gmail.com>
2023-05-31 21:55:20 +0800

    update

--- 07.mobility.py
+++ 07.mobility.py
@@ -17,13 +17,19 @@ plt.rcParams["font.family"] = "Times New Roman"
 figpath = '/Users/chingchen/Desktop/figure/StagYY/'
 datapath = '/Users/chingchen/Desktop/data/'
 path = '/Users/chingchen/Desktop/model/'
-model = 'w0225'
-model_list = ['w0204','w0216']#,'w0228']
-model_list = ['w0207','w0208','w0209']
-model_list = ['w0219','w0220','w0221']
+model = 'w1009'
+model_list = ['w0204','w0802','w0216']#,'w0228']
+model_list = ['w1009']
+# model_list = ['w0219','w0220','w0221']
+# model_list = ['w0201','w0204','w0207']
+# model_list = ['w0213','w0216','w0219','w0222']
+# model_list = ['w0801','w0802','w0803','w0804']
+# model_list = ['w0201','w0801','w0213']#,'w0225']
+label_list = ['Ra = 1e5','Ra = 3.2e4','Ra = 1e4','Ra = 1e3']
 labelsize = 30
 bwith = 3
 fig_mobility = 1
+surface_velocity=0
 
 newcolors = ['#2F4F4F','#4682B4','#CD5C5C','#708090',
               '#AE6378','#282130','#7E9680','#24788F',
@@ -49,48 +55,46 @@ header_list = ['r','Tmean','Tmin','Tmax','vrms','vmin','vmax',
                'metalmax','gsmean','gsmin','gsmax','viscdisslog',
                'viscdissmin','viscdissmax','advtot','advdesc',
                'advasc','tcondmean','tcondmin','tcondmax']
-file = datapath+model+'_time.dat'
-ff = pd.read_csv(file,sep = '\\s+')  
-end = 100
+
+end = 1500
 if fig_mobility:
     fig4,(ax4) = plt.subplots(1,1,figsize=(12,8))
     kk = 3
-    vrms = ff.Vrms
-    time = ff.time
     for kk, model in enumerate(model_list):
+        file = datapath+model+'_time.dat'
+        ff = pd.read_csv(file,sep = '\\s+')  
+        time_vrms = np.average(ff.Vrms.tolist()[-500:])
         vsurf = np.zeros(end-1)
         vrms = np.zeros(end-1)
         snaptime = np.zeros(end-1)
-        average_vrms = np.zeros(end-1)
         for uu in range(1,end):
             ffsnap = pd.read_csv(path+model+'/datafile/'+model+'_data_'+str(uu)+'.txt',
                           sep = '\\s+',header = None,names = header_list)
             vsurf[uu-1] = ffsnap.vhrms[127]
-            #vrms[uu-1] = ffsnap.vrms[0]
-            average_vrms[uu-1] = np.average(ffsnap.vrms)
             vrms[uu-1] = np.average(ffsnap.vrms)
             snaptime[uu-1] = uu/1000
-        #ax4.plot(snaptime,vsurf/vrms,color = '#849DAB',lw=5)
-        ax4.plot(snaptime,vsurf/vrms,color = newcolors[kk],lw=5)
-        
-    #ax4.set_xlim(0-.01,0.5)
-    ax4.set_ylim(0.0,0.5)
-    #for qq in range(len(time)):
-        #math.floor(time[qq]*1000)/1000
-        
-    #ax4.plot(ff.time, ff.Vrms,color = newcolors[kk],lw=5,label = 'time.dat')
-    #ax4.plot(snaptime[snaptime>0],average_vrms[snaptime>0],color = newcolors[kk+1],lw=5,label = 'rprof.dat')
-    # ax3.plot(ff.time, ff.Tmean,color = newcolors[kk+1],lw=3,label = 'F_bot')
-    
-    ax4.tick_params(axis='x', labelsize=labelsize)
-    ax4.tick_params(axis='y', labelsize=labelsize)
-    ax4.spines['bottom'].set_linewidth(bwith)
-    ax4.spines['top'].set_linewidth(bwith)
-    ax4.spines['right'].set_linewidth(bwith)
-    ax4.spines['left'].set_linewidth(bwith)
+        ax4.plot(snaptime,vsurf/time_vrms,color = newcolors[kk],lw=5,label=label_list[kk]) #time.dat
+        # ax4.plot(snaptime,vsurf/vrms,color = newcolors[kk],lw=5,label=model) #rprof
+    ax4.set_ylim(0.0,0.01)
+    ax4.tick_params(labelsize=labelsize)
+    for axis in ['top','bottom','left','right']:
+        ax4.spines[axis].set_linewidth(bwith)
     ax4.grid()
-    # ax.set_xlim(0.09,0.12)
-    # ax.set_ylim(7,8.2)
     ax4.set_xlabel('time',fontsize = labelsize)
-    #ax4.set_ylabel('Vrms',fontsize = labelsize)
     ax4.legend(fontsize = 20)
+    ax4.set_ylabel('Mobility',fontsize=labelsize)
+if surface_velocity:
+    fig5,(ax6) = plt.subplots(1,1,figsize=(12,4))
+    vsurf = np.zeros(end-1)
+    for uu in range(1,end):
+        ffsnap = pd.read_csv(path+model+'/datafile/'+model+'_data_'+str(uu)+'.txt',
+                      sep = '\\s+',header = None,names = header_list)
+        vsurf[uu-1] = ffsnap.vhrms[127]
+        snaptime[uu-1] = uu/1000
+    ax6.plot(snaptime,vsurf,color = newcolors[kk],lw=5,label=model)
+    # ax6.set_ylim(0.9,1)
+    # ax4.set_xlim(0,10)
+    ax6.tick_params(labelsize=labelsize)
+    for axis in ['top','bottom','left','right']:
+        ax6.spines[axis].set_linewidth(bwith)
+    ax6.grid()
