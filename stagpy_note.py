7f17e0074ccb4a47d59a64b6fc4841717340ca5d
chingchenn <ching602504@gmail.com>
2023-07-09 13:41:02 +0800

    update stagpy

--- stagpy_note.py
+++ stagpy_note.py
@@ -1,8 +1,9 @@
 import stagpy
+from stagpy import field
 from stagpy import stagyydata
-sdat = stagyydata.StagyyData('/lfs/jiching/w0201/') # model path
+sdat = stagyydata.StagyyData('/lfs/jiching/thermo_chemical/TC_2D-SPH_Basalt_HR002/') # model path
 sdat.__dir__() # 
-#for ww in 
+
 print(sdat.scales) # acceleration, density, dyn_visc,
 # heat_flux, heat_production,length, power,
 # sp_heat, stress, temperature, th_cond, th_diff,
@@ -11,3 +12,4 @@ sdat.refstate #  adiabats [two matrix], systems [one matrix]
 sdat.tseries # at_step(setp)[29 個參數], isteps[], 
 
 sdat.par['refstate'] # find input parameters
+print(field.get_meshes_fld(sdat.snaps[15],'T'))
