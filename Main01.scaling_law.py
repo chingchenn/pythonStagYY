130b0b20621dd1f321203865575650371d1b4d81
chingchenn <ching602504@gmail.com>
2023-12-30 12:09:21 +0800

    full SOP of scaling pre-process

--- Main01.scaling_law.py
+++ Main01.scaling_law.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 21 14:22:28 2023
+Created on Fri Dec  29 16:44:37 2023
 
 @author: chingchen
 """
-
-import os
-import numpy as np
+import sys
 import pandas as pd
-import function_savedata as fs
-import matplotlib.pyplot as plt
+import numpy as np
+import time
 from pandas.core.frame import DataFrame
+import matplotlib.pyplot as plt
+import function_savedata as fs
 
 
-### PATH ###
+labelsize = 20
+bwith = 3
 local = 1
+
 if local:
+    figpath = '/Users/chingchen/Desktop/figure/StagYY/'
     path = '/Users/chingchen/Desktop/data/'
     workpath = '/Users/chingchen/Desktop/StagYY_Works/'
     modelpath = '/Users/chingchen/Desktop/model/'
 else:
-    path = '/lfs/jiching/data/'
-    workpath = '/lfs/jiching/ScalingLaw_model/'
-    modelpath = '/lfs/jiching/ScalingLaw_model/'
     figpath = '/lfs/jiching/figure/'
+    path = '/lfs/jiching/ScalingLaw_model/data_scaling/'
+    workpath = '/lfs/jiching/ScalingLaw_model/'
+    modelpath = '/lfs/jiching/ScalingLaw_model/23agu/'
+model = sys.argv[1]
 
-### SETTING ###
+tw1 = 0.2
+tw2 = 0.24
+xmin,xmax=2.6,9.3
+model_information = pd.read_csv(workpath+'model_information_23end.csv',sep=',')  
 
-time_shift = 0.01
-time_window = 0.1
+fig_ftime = 1
+choosen_time_window = 1
+plot_average = 1
+plot_Tprofile = 0
+save = 1
 
-newcolors = ['#2F4F4F','#4682B4','#CD5C5C','#708090',
-              '#AE6378','#282130','#7E9680','#24788F',
-              '#849DAB','#EA5E51','#35838D','#4198B9',
-              '#414F67','#97795D','#6B0D47','#A80359','#52254F']
+
 rprof_header_list = ['r','Tmean','Tmin','Tmax','vrms','vmin','vmax',
                'vzabs','vzmin','vzmax','vhrms','vhmin','vhmax',
                'etalog','etamin','etamax','elog','emin','emax',
@@ -49,272 +56,133 @@ rprof_header_list = ['r','Tmean','Tmin','Tmax','vrms','vmin','vmax',
                'viscdissmin','viscdissmax','advtot','advdesc',
                'advasc','tcondmean','tcondmin','tcondmax']
 
-plt.rcParams["font.family"] = "Times New Roman"
-model_information = pd.read_csv(workpath+'model_information.csv',sep=',') 
-labelsize = 20
-bwith = 3
-scatter_size = 100
+for jj in range(len(model_information)):
+    if model_information.model[jj]== model:
+        break 
+
+f = model_information.f[jj]
+Ea = model_information.Ea[jj]
+Ra0 = model_information.Ra0[jj]
+H = model_information.H[jj]
+
+ff = pd.read_csv(path+model+'_scaling_time_data.csv')
+kk = np.arange(0.03,2.5,0.005) # time shift = 0.005
+tt1=np.zeros(len(kk))
+tt2=np.zeros(len(kk))
+anb=np.zeros(len(kk))
+ant=np.zeros(len(kk))
+afb=np.zeros(len(kk))
+aft=np.zeros(len(kk))
 
 
-### DO WHAT ###
-
-split_dat = 0
-save_data = 0
-plot_average = 1
-running_average = 1
-plot_scaling_result = 0
-fig_compare_T = 0
-fig_compare_Fu = 0
-fig_compare_Nu = 0
-
-
-if split_dat:
-    for jj in range(len(model_information)):
-        model = model_information.model[jj]
-        file = path+model+'_rprof.dat'
-        sourceFileName = file
-        sourceFileData = open(file,'r')
-        ListOfLine = sourceFileData.read().splitlines()
-        new_data = ListOfLine[1:]
-        n = len(new_data)
-        p = 129
-        m = int(n/p)
-        for i in range(m):
-            destFileName = modelpath+model+'/datafile/'
-            if not os.path.isdir(modelpath+model):
-                os.mkdir(modelpath+model)
-            if not os.path.isdir(destFileName):
-                os.mkdir(destFileName)
-            destFileData = open(destFileName+model+'_data_'+str(i)+'.txt','w')
+if fig_ftime:
+    fig,(ax,ax2) = plt.subplots(2,1,figsize=(12,10))
+    Ftop_pred = f**2 * ff.F_bot + (1+f+f**2)/3 *H
+    ax.plot(ff.time,Ftop_pred,color='#AE6378',label = 'F_bot')
+    ax.plot(ff.time,ff.F_top,color='#35838D',label = 'F_top',lw=3)
+    ax.set_title(model,fontsize=labelsize)
+    ax.set_ylabel('F$_{top}$',fontsize = labelsize)
+    
+if choosen_time_window:
+    for ii,tt in enumerate(kk): 
+        time_window1=tt
+        time_window2=tt+0.005 # time window = 0.1
+        if len(ff.F_bot[(ff.time>time_window1) & (ff.time<time_window2)])==0:
+            break
+        average_fl_top = np.median(ff.F_top[(ff.time>time_window1) & (ff.time<time_window2)])
+        tt1[ii] = np.round(time_window1,2)
+        tt2[ii] = np.round(time_window1,2)
+        aft[ii] = average_fl_top
+    
+    tt1 = tt1[aft>0]
+    tt2 = tt2[aft>0]
+    aft = aft[aft>0]
+    if len(tt1[(aft>np.mean(aft)+np.std(aft))])==0:
+        print(model)
+    
+    time_window1=np.max(tt1[(aft>np.mean(aft)+0.1*np.std(ff.Nu_bot))])+0.001 # minimum time + 0.3
+    time_window2=np.max(tt2[(aft<np.mean(aft)+0.1*np.std(ff.Nu_top))])-0.001 # maximum time - 0.05
+    #time_window1 = tw1
+    #time_window2 = tw2
+    
+    if plot_average:
+        ax2.scatter(tt1,aft,color='#35838D')
+        ax2.set_ylabel('F$_{bot}$',fontsize = 20)
+        ax.legend(fontsize =labelsize)
+        for aa in [ax,ax2]:
+            aa.set_xlim(0,time_window2+0.05)
+            aa.tick_params(labelsize=labelsize)
+            aa.set_ylim(xmin,xmax)
+            aa.axvline(x=time_window1,color = '#97795D')
+            aa.axvline(x=time_window2,color = '#6B0D47')
+            for axis in ['top','bottom','left','right']:
+                aa.spines[axis].set_linewidth(bwith)
             
-            if (i==m-1):
-                for line in new_data[i*p+1:]:
-                    destFileData.write(line+'\n')
-            else:
-                for line in new_data[i*p+1:(i+1)*p]:
-                    destFileData.write(line+'\n')
-            destFileData.close()
-        print(model+'==DONE==',m-1)
-if running_average: # Using the moving window to choose the time window 
-    # savedata array (model number length)
-    leng = len(model_information)
-    model_array = np.zeros(leng)
-    time_window1_array = np.zeros(leng)
-    time_window2_array = np.zeros(leng)
-    average_Nu_bot_array = np.zeros(leng)
-    average_Nu_top_array = np.zeros(leng)
-    average_fl_bot_array = np.zeros(leng)
-    average_fl_top_array = np.zeros(leng)
-    average_t_array = np.zeros(leng)
-    lid_list = np.zeros(leng)
-    for jj in range(len(model_information)):
-        model = model_information.model[jj]
-        f = model_information.f[jj]
-        Ea = model_information.Ea[jj]
-        Ra0 = model_information.Ra0[jj]
-        file = path+model+'_time.dat'
-        ff = pd.read_csv(file,sep = '\\s+') 
-        # time series array (window length)
-        window = np.arange(0.0,2.5,time_shift) # time_shift = 0.01
-        tt1 = np.zeros(len(window))
-        tt2 = np.zeros(len(window))
-        anb = np.zeros(len(window))
-        afb = np.zeros(len(window))
-        for ii,tt in enumerate(window): 
-            time_window1 = tt
-            time_window2 = tt + time_window # time_window = 0.1
-            if len(ff.F_bot[(ff.time>time_window1) * (ff.time<time_window2)])==0:
-                break
-            average_Nu_bot = np.median(ff.Nu_bot[(ff.time>time_window1) * (ff.time<time_window2)])
-            average_fl_bot = np.median(ff.F_bot[(ff.time>time_window1) * (ff.time<time_window2)])
-            tt1[ii] = np.round(time_window1,2)
-            tt2[ii] = np.round(time_window1,2)
-            anb[ii] = average_Nu_bot
-            afb[ii] = average_fl_bot
-    
-        tt1 = np.array(tt1)
-        tt2 = np.array(tt2)
-        if len(tt1[(anb>np.mean(anb)+np.std(anb))])==0:
-            print('========== '+model+' Cannot find suitble time window ==========')
-            continue
-        time_window1=np.max(tt1[(anb>np.mean(anb)+np.std(anb))])+0.01 # minimum time + 0.01
-        time_window2=np.max(tt2[(anb<np.mean(anb)+np.std(anb))]) # maximum time - 0.5
-        if plot_average:
-            fig,(ax,ax2) = plt.subplots(2,1,figsize=(12,10))
-            ax.set_title(model,fontsize=labelsize)
-            ax.scatter(tt1,anb,color='#35838D')
-            ax2.scatter(tt1,afb,color='#35838D')
-            for aa in [ax,ax2]:
-                aa.tick_params(labelsize=labelsize)
-                aa.set_xlim(0,2.5)
-                for axis in ['top','bottom','left','right']:
-                    aa.spines[axis].set_linewidth(bwith)
-    
-        
-#       # calculate the average Nu and heat flow in given time window    
-        average_Nu_bot = np.average(ff.Nu_bot[(ff.time>time_window1)*(ff.time<time_window2)])
-        average_Nu_top = np.average(ff.Nu_top[(ff.time>time_window1)*(ff.time<time_window2)])
-        average_fl_bot = np.average(ff.F_bot[(ff.time>time_window1)*(ff.time<time_window2)])
-        average_fl_top = np.average(ff.F_top[(ff.time>time_window1)*(ff.time<time_window2)])
-        ### Find Average Temperature and the Stagnant Lid Thickness
-        Tmm = np.zeros(abs(int(time_window1*100)-int(time_window2*100)))
-        new_thickness = np.zeros(abs(int(time_window1*100)-int(time_window2*100)))
-        for ss,time in enumerate(range(int(time_window1*100),int(time_window2*100))):
-            rprof_ff = pd.read_csv(modelpath+model+'/datafile/'+model+'_data_'+str(time)+'.txt',
-                          sep = '\\s+',header = None,names = rprof_header_list) 
-            Tmm[ss] = np.average(rprof_ff.Tmean[(rprof_ff.r>0.4)*(rprof_ff.r<0.5)])
-            x = np.array(rprof_ff.vzabs*rprof_ff.Tmean)
-            y = np.array(rprof_ff.r)
-            smooth_d2 = np.gradient(np.gradient(x))
-            infls = np.where(np.diff(np.sign(smooth_d2)))[0]
-            if len(x[infls])!=0:
-                inf_point = x[infls][-1]
-                index_inf_point = [i for i, kk in enumerate(x) if kk == inf_point][0]
-                a = (y[index_inf_point]-y[index_inf_point+1])/(x[index_inf_point]-x[index_inf_point+1])
-                b = y[infls][-1]-a*inf_point
-                line_x = np.linspace(0,2400)
-                line_y = a*line_x + b
-                new_thickness[ss] = line_y[0]
-        average_t = Tmm[-5]
-        lid = np.average(new_thickness)
-    
-        if save_data:
-            time_window1_array[jj] = time_window1
-            time_window2_array[jj] = time_window2
-            average_Nu_bot_array[jj] = average_Nu_bot 
-            average_Nu_top_array[jj] = average_Nu_top 
-            average_fl_bot_array[jj] = average_fl_bot
-            average_fl_top_array[jj] = average_fl_top
-            average_t_array[jj]=average_t
-            lid_list[jj] = lid
-    
-    if save_data:         
-        moodel=DataFrame(model_information.model,columns=['model'])
-        f=DataFrame(model_information.f,columns=['f'])
-        Ea=DataFrame(model_information.Ea,columns=['Ea'])
-        Ra=DataFrame(model_information.Ra0,columns=['Ra0'])
-        tt1=DataFrame(time_window1_array,columns=['time_window1'])
-        tt2=DataFrame(time_window2_array,columns=['time_window2'])
-        lid=DataFrame(lid_list,columns=['lid'])
-        aaa=DataFrame(average_Nu_bot_array,columns=['average_Nu_bot'])
-        bbb=DataFrame(average_Nu_top_array,columns=['average_Nu_top'])
-        ccc=DataFrame(average_fl_bot_array,columns=['average_fl_bot'])
-        ddd=DataFrame(average_fl_top_array,columns=['average_fl_top'])
-        fff=DataFrame(average_t_array,columns=['Tm'])
-        model = np.array(DataFrame(model_information.model,columns=['model']))
-        ra = np.array(model_information.Ra0)
-        gamma = np.log(np.array(model_information.Ea))
-        nu = average_Nu_bot_array
-        xx1 = np.ones(len(nu))*1e-5
-        xx2 = np.ones(len(nu))*1e-3
-        thetam = (average_t_array)
-        rsurf = ra/np.exp(gamma/2)
-        #fs.save_6txt('scalinglaw_inversion',path,rsurf,gamma,thetam,xx1,nu,xx2)
-        fs.save_3txt('test_Tm',workpath,model,rsurf,thetam)
-#        n6 = pd.concat([moodel,f,Ra,Ea,tt1,tt2,lid,aaa,bbb,ccc,ddd,fff],axis=1)
-#        n6.to_csv(path+'average_data'+'.csv',index=False)
-x=[]
-y=[]
-if plot_scaling_result:
-    fig5,(ax7) = plt.subplots(1,1,figsize=(7,7))
-    # fig8,(ax8) = plt.subplots(1,1,figsize=(7,7))
-    model_data = pd.read_csv(path+'average_data'+'.csv') 
-    for jj in range(len(model_data)):
-        model = model_data.model[jj]
-        f = model_data.f[jj]
-        Ea = model_data.Ea[jj]
-        Ra0 = model_data.Ra0[jj]
-        time_window1 = model_data.time_window1[jj]
-        time_window2 = model_data.time_window2[jj]
-        average_Nu_bot = model_data.average_Nu_bot[jj]
-        average_fl_bot = model_data.average_fl_bot[jj]
-        average_t = model_data.Tm[jj]
-        
-        ## Setting shape of points to distingish the f 
-        # if f==0.3:
-        #     marker ="^"
-        # elif f==0.5:
-        #     marker ="s"
-        if f==0.3 or f == 0.5 or f ==0.7 or f ==0:
-            continue
-        # elif f==0.7:
-        #     marker='o'
-        elif f == 0.8:
-            marker='p'
-        ## Setting colour of points to distingish the Ea
-        if Ea ==1e5:
-            kk = 2
-            label='1e5'
-        elif Ea ==1e6:
-            kk = 0
-            label='1e6'
-        elif Ea ==1e7:
-            kk = 8
-            label='1e7'
-        elif Ea ==1e8:
-            kk = 1
-            label='1e8'    
+# calculate the average Nu and heat flow in given time window    
+average_fl_bot = np.average(ff.F_bot[(ff.time>time_window1)&(ff.time<time_window2)])
+average_fl_top = np.average(ff.F_top[(ff.time>time_window1)&(ff.time<time_window2)])
 
-        plt.rcParams['font.size'] = 20
-        if average_Nu_bot>0:
-            print(model,Ra0,Ea/1e5,average_Nu_bot)
-            if fig_compare_Fu :
-                if model=='w0201' or model=='w0204' or model=='w0207'or model=='w0210':
-                    ax7.scatter(Ra0,average_fl_bot*(f**2),color=newcolors[kk+1],label = label,s=scatter_size,marker=marker)
-                else:
-                    ax7.scatter(Ra0,average_fl_bot*(f**2),color=newcolors[kk+1],s=scatter_size,marker=marker)
-                ax7.set_ylabel('F_bot',fontsize = labelsize)
-                ax7.set_yscale('log')
-                
-            if fig_compare_Nu :
-                if model=='w0201' or model=='w0204' or model=='w0207'or model=='w0210' or model=='w0812' or model=='w0811' or model=='w0810' or model=='w0809':
-                    ax7.scatter(Ra0,average_Nu_bot,color=newcolors[kk+1],label = label,s=scatter_size,marker=marker)
-                else:
-                    ax7.scatter(Ra0,average_Nu_bot,color=newcolors[kk+1],s=scatter_size,marker=marker)
-                ax7.set_ylabel('Nu',fontsize = labelsize)
-                ax7.set_yscale('log')
-                
-            if fig_compare_T :
-                if model=='w0201' or model=='w0204' or model=='w0207'or model=='w0210':
-                    ax7.scatter(Ra0,average_t,color=newcolors[kk+1],label = label,s=scatter_size,marker=marker)
-                else:
-                    ax7.scatter(Ra0,average_t,color=newcolors[kk+1],s=scatter_size,marker=marker)
-                ax7.set_ylabel('Temp',fontsize = labelsize)
-        
-            x.append(Ra0)
-            y.append(average_Nu_bot)
-        # if Ra0==1e5:
-        #     kk = 2
-        #     label='1e5'
-        # elif Ra0==3.2e4:
-        #     kk = 0
-        #     label='3.2e4'
-        # elif Ra0==5.6e4:
-        #     kk = 10
-        #     label='5.6e4'
-        # elif Ra0 ==1e4:
-        #     kk = 4
-        #     label='1e4' 
-        # else:
-        #     kk = 8
-        #     label = 'nuknow'
-        # if model=='w0201' or model=='w0204' or model=='w0207'or model=='w0210':
-        #     ax8.scatter(Ea,average_t,color=newcolors[kk+1],label = label,s=scatter_size,marker=marker)
-        # else:
-        #     ax8.scatter(Ea,average_t,color=newcolors[kk+1],s=scatter_size,marker=marker)
 
+#-----------Find Average Temperature and the Stagnant Lid Thickness------------
+end = int((time_window2)*1000)
+new_thickness = np.zeros(end)
+avg_temp = np.zeros(end)
+
+for i in range(200,end+1):
+    rof = pd.read_csv(workpath+model+'/datafile/'+model+'_data_'+str(i)+'.txt',
+                      sep = '\\s+',header = None,names = rprof_header_list)    
+    x = np.array(rof.vzabs*rof.Tmean)
+    y = np.array(rof.r)
+    smooth_d2 = np.gradient(np.gradient(x))
+    infls = np.where(np.diff(np.sign(smooth_d2)))[0]
+ 
+    if len(x[infls])!=0:
+        inf_point = x[infls][-1]
+        index_inf_point = [i for i, kk in enumerate(x) if kk == inf_point][0]
+        a = (y[index_inf_point]-y[index_inf_point+1])/(x[index_inf_point]-x[index_inf_point+1])
+        b = y[infls][-1]-a*inf_point
+        line_x = np.linspace(0,4000)
+        line_y = a*line_x + b
+        new_thickness[i-1] = line_y[0]
+        avg_temp[i-1] = np.average(rof.Tmean[y<y[x==inf_point]])
+
+rprof_ff = pd.read_csv(modelpath+model+'/datafile/'+model+'_data_'+str(end)+'.txt',
+              sep = '\\s+',header = None,names = rprof_header_list) 
+Tmm = np.average(rprof_ff.Tmean[(rprof_ff.r>0.3)*(rprof_ff.r<0.5)])
+average_t = Tmm
+dlid = 1-np.average(new_thickness[20:])
+ftop = average_fl_top
+fbot = average_fl_bot
+gamma = np.log(np.array(Ea))
+rsurf = Ra0/np.exp(gamma/2)
+raeff = rsurf*np.exp(gamma*average_t)
+flid = 1-(1-f)*dlid
+Tlid = dlid/flid*(ftop-1/6*H*(2-flid-flid**2)/(1-f))
+Ur = (1+f+f**2)/3*H/ftop
+#print(model,rsurf,gamma,average_t,1e-5,average_Nu_bot,1e-3)
+print('##############################################################')
+print(model,round(rsurf,5),f,format(Ea,'.1e'),H,round(Tmm,3),round(ftop,3),
+      round(fbot,3),round(Ur,3),format(raeff,'.3e'),round(dlid,3),round(Tlid,3))
+print(round(ftop,3),round(f**2 * fbot + (1+f+f**2)/3 *H,3),round(f**2 * fbot + (1+f+f**2)/3 *H-ftop,3))
+
+
+ax.set_ylim(ftop-0.2,ftop+0.2)
+ax2.set_ylim(ftop-0.2,ftop+0.2)
+if plot_Tprofile:
+    fig3,(ax6) = plt.subplots(1,1,figsize=(6,10))
+    ax6.plot(rprof_ff.Tmean,rprof_ff.r,color ='#414F67',lw=3)
+    ax6.axvline(x=Tmm,color ='#97795D')
+    for aa in [ax6]:
+        aa.set_xlim(0.85,1.05)
+        aa.tick_params(labelsize=labelsize)
+        aa.set_ylim(0,1)
         for axis in ['top','bottom','left','right']:
-            ax7.spines[axis].set_linewidth(bwith)
-            # ax8.spines[axis].set_linewidth(bwith)
-    
-        # ax7.grid()
-        ax7.set_xscale('log')
-        # ax7.set_yscale('log')
-        
-        ax7.legend(fontsize=labelsize)
-        ax7.set_xlabel('Ra0',fontsize = labelsize)
-        
-        # ax7.set_xticks(np.logspace(3,5,3))
-        # ax7.set_yticks(np.arange(0, 20, 1))
-        ax7.tick_params(axis='both', which='major',labelsize=labelsize)
-        # ax7.set_ylim(1,10)
+            aa.spines[axis].set_linewidth(bwith)
+
+if save:
+    savearray = np.array([round(rsurf,5),f,round(Ea,0),H,round(Tmm,3),round(ftop,3),
+          round(fbot,3),round(Ur,3),round(raeff,3),round(dlid,3),round(Tlid,3)])
+    fs.save_1txt(model+'_scaling_grep_data',path,savearray)
+
+
+
