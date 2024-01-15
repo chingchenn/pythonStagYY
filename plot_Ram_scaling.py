4aa12db95a510cb47d0b79d4283c53f67389dc6a
chingchenn <ching602504@gmail.com>
2024-01-15 08:17:59 +0800

    update

--- plot_Ram_scaling.py
+++ plot_Ram_scaling.py
@@ -12,8 +12,6 @@ import matplotlib.pyplot as plt
 
 workpath = '/Users/chingchen/Desktop/StagYY_Works/'
 
-
-fig6 = 1 ## H models top
 a = 1.33
 b = 0.33
 c = 1.70
@@ -31,48 +29,49 @@ sigma_a = 0.6
 sigma_b = 0.01
 sigma_c = 0.1
 sigma_F = 0.01
-if fig6:
-    fig6,(ax) = plt.subplots(1,1,figsize=(12,9))
-    labelsize=16
-    file = 'plot_Ram.dat'
-    Ra0_array, f_array, H_array, Deta_array, Tm_array, Ftop_array = np.loadtxt(workpath+file).T
-    Ra_model = np.logspace(4,10)
-    for kk in range(len(Ra0_array)):
-        Ra0 = Ra0_array[kk]
-        f = f_array[kk]
-        H = H_array[kk]
-        Deta = np.exp(Deta_array[kk])
-        Ftop=Ftop_array[kk]   
-        gamma=np.log(Deta)
-        
-        Tm = Tm_array[kk] 
-        Ram = Ra0*np.exp(gamma*Tm) 
-        y = Ftop*(gamma**c)
-        x = Ram
-        
-        yerror = sigma_F/Ftop* y + sigma_c * y * np.log(gamma)
-        if H_array[kk]>0:
-            ax.errorbar(x,y,yerr = yerror,fmt='ok',ecolor="r",label='mix heat' if kk==12 else '') 
-        else:
-            ax.errorbar(x,y,yerr = yerror,fmt='og',ecolor="r",label='basal heat' if kk==0 else '') 
-        
-        
-    F_pred = a * Ra_model **b
-    x_pred = Ra_model
-    ax.plot(x_pred,F_pred,c='b')
-    error_plus = (a+sigma_a) * Ra_model **(b+sigma_b)
-    error_minus = (a-sigma_a) * Ra_model **(b-sigma_b)
-    ax.plot(x_pred,error_plus,c = 'k',lw = 1, linestyle = 'dashed')
-    ax.plot(x_pred,error_minus,c = 'k',lw = 1, linestyle = 'dashed')
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.set_xlim(1e5,1e9)
-    ax.set_ylim(1e2,5e3)
-    #ax.legend(fontsize=labelsize)
-    ax.set_xlabel('Ram',fontsize = labelsize)
-    ax.set_ylabel('F_top',fontsize = labelsize)
-    ax.tick_params(labelsize=labelsize)
-    ax.set_title('normal models with ftop data and ftop inverse',fontsize = 16)
-    ax.grid()
-    ax.legend(fontsize = labelsize)
+
+fig6,(ax) = plt.subplots(1,1,figsize=(12,9))
+labelsize=16
+file = 'plot_Ram.dat'
+Ra0_array, f_array, H_array, Deta_array, Tm_array, Ftop_array = np.loadtxt(workpath+file).T
+Ra_model = np.logspace(4,10)
+for kk in range(len(Ra0_array)):
+    Ra0 = Ra0_array[kk]
+    f = f_array[kk]
+    H = H_array[kk]
+    Deta = np.exp(Deta_array[kk])
+    Ftop=Ftop_array[kk]   
+    gamma=np.log(Deta)
+    
+    Tm = Tm_array[kk] 
+    Ram = Ra0*np.exp(gamma*Tm) 
+    y = Ftop*(gamma**c)
+    x = Ram
+    
+    yerror = sigma_F/Ftop* y + sigma_c * y * np.log(gamma)
+    if H_array[kk]>0:
+        # ax.errorbar(x,y,yerr = yerror,fmt='ok',ecolor="r",label='mix heat' if kk==12 else '') 
+        ax.errorbar( x,y,yerr = yerror, fmt='o', capsize=5,color = 'orange',ecolor='#849DAB',label='mix heat' if kk==12 else '')
+    else:
+        # ax.errorbar(x,y,yerr = yerror,fmt='og',ecolor="r",label='basal heat' if kk==0 else '') 
+        ax.errorbar(x,y,yerr = yerror, fmt='o', capsize=5,color = 'k',ecolor='#849DAB',label='basal heat' if kk==0 else '')
+    
+F_pred = a * Ra_model **b
+x_pred = Ra_model
+ax.plot(x_pred,F_pred,c='b')
+error_plus = (a+sigma_a) * Ra_model **(b+sigma_b)
+error_minus = (a-sigma_a) * Ra_model **(b-sigma_b)
+ax.plot(x_pred,error_plus,c = 'k',lw = 1, linestyle = 'dashed')
+ax.plot(x_pred,error_minus,c = 'k',lw = 1, linestyle = 'dashed')
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlim(1e5,1e9)
+ax.set_ylim(1e1,5e3)
+#ax.legend(fontsize=labelsize)
+ax.set_xlabel('Ram',fontsize = labelsize)
+ax.set_ylabel('F_top',fontsize = labelsize)
+ax.tick_params(labelsize=labelsize)
+ax.set_title('normal models with ftop data and ftop inverse',fontsize = 16)
+ax.grid()
+ax.legend(fontsize = labelsize)
 ####################################################################################################
